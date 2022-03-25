b389403905cb020213034461b26c6c8c35793c30
Misaka No <96560028+misakano7545@users.noreply.github.com>
2022-03-25 22:08:09 +0800

    Update screen.sh

--- screen.sh
+++ screen.sh
@@ -1,40 +1,79 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-green(){
-    echo -e "\033[32m$1\033[0m";
+red() {
+    echo -e "\033[31m\033[01m$1\033[0m"
 }
 
-red(){
-    echo -e "\033[31m$1\033[0m";
+green() {
+    echo -e "\033[32m\033[01m$1\033[0m"
 }
 
-yellow(){
-    echo -e "\033[33m$1\033[0m";
+yellow() {
+    echo -e "\033[33m\033[01m$1\033[0m"
 }
 
-white(){
-    echo -e "\033[37m$1\033[0m"
-}
+REGEX=("debian" "ubuntu" "centos|red hat|kernel|oracle linux|alma|rocky" "'amazon linux'")
+RELEASE=("Debian" "Ubuntu" "CentOS" "CentOS")
+PACKAGE_UPDATE=("apt -y update" "apt -y update" "yum -y update" "yum -y update")
+PACKAGE_INSTALL=("apt -y install" "apt -y install" "yum -y install" "yum -y install")
+PACKAGE_UNINSTALL=("apt -y autoremove" "apt -y autoremove" "yum -y autoremove" "yum -y autoremove")
 
-blue(){
-    echo -e "\033[36m$1\033[0m";
-}
+[[ $EUID -ne 0 ]] && red "请在root用户下运行脚本" && exit 1
 
-[[ $(type -P yum) ]] && yumapt='yum -y' || yumapt='apt -y'
-[[ $(type -P screen) ]] || (yellow "screen未安装，正在安装中" && $yumapt install screen)	   
+CMD=("$(grep -i pretty_name /etc/os-release 2>/dev/null | cut -d \" -f2)" "$(hostnamectl 2>/dev/null | grep -i system | cut -d : -f2)" "$(lsb_release -sd 2>/dev/null)" "$(grep -i description /etc/lsb-release 2>/dev/null | cut -d \" -f2)" "$(grep . /etc/redhat-release 2>/dev/null)" "$(grep . /etc/issue 2>/dev/null | cut -d \\ -f1 | sed '/^[ ]*$/d')")
 
-function back(){
-    echo "设置完成，请选择接下来的操作"
-    echo "1. 回到主页"
-    echo "2. 退出脚本"
-    read -p "请输入选项:" backInput
-    case "$backInput" in 
-        1 ) menu ;;
-        2 ) exit 0
+for i in "${CMD[@]}"; do
+    SYS="$i" && [[ -n $SYS ]] && break
+done
+
+for ((int = 0; int < ${#REGEX[@]}; int++)); do
+    [[ $(echo "$SYS" | tr '[:upper:]' '[:lower:]') =~ ${REGEX[int]} ]] && SYSTEM="${RELEASE[int]}" && [[ -n $SYSTEM ]] && break
+done
+
+[[ -z $SYSTEM ]] && red "不支持当前VPS系统，请使用主流的操作系统" && exit 1
+
+[[ -z $(type -P screen) ]] && ${PACKAGE_UPDATE[int]} && ${PACKAGE_INSTALL[int]} screen
+
+back2menu() {
+    green "所选操作执行完成"
+    read -p "请输入“y”退出，或按任意键回到主菜单：" back2menuInput
+    case "$back2menuInput" in
+        y) exit 1 ;;
+        *) menu ;;
     esac
 }
 
-function menu(){
+createScreen(){
+    read -p "设置screen后台名称：" screenName
+    screen -S $screenName
+    back2menu
+}
+
+enterScreen(){
+    screenNames=`screen -ls | grep '(Detached)' | awk '{print $1}' | awk -F "." '{print $2}'`
+    [[ -n $screenNames ]] && yellow "当前运行的Screen会话如下所示：" && green "$screenNames"
+    read -p "输入进入的screen后台名称：" screenName
+    screen -r $screenName || red "没有找到 $screenName 的会话"
+    back2menu
+}
+
+deleteScreen(){
+    screenNames=`screen -ls | grep '(Detached)' | awk '{print $1}' | awk -F "." '{print $2}'`
+    [[ -n $screenNames ]] && green "$screenNames"
+    read -p "输入删除的screen后台名称：" screenName
+    screen -S $screenName -X quit || red "没有找到 $screenName 的会话"
+    back2menu
+}
+
+killAllScreen(){
+    screenNames=`screen -ls | grep '(Detached)' | awk '{print $1}' | awk -F "." '{print $2}'`
+    screen -wipe
+    [[ -n $screenNames ]] && screen -ls | grep '(Detached)' | cut -d. -f1 | awk '{print $1}' | xargs kill
+    green "所有screen后台会话已清除完毕" || red "没有任何screen后台会话"
+    back2menu
+}
+
+menu(){
     clear
     red "=================================="
     echo "                           "
@@ -44,32 +83,19 @@ function menu(){
     red "  Site: https://owo.misaka.rest  "
     echo "                           "
     red "=================================="
-    echo "1. 创建screen并设置后台名称"
-    echo "2. 查看并进入指定screen后台"
-    echo "3. 查看并删除指定screen后台"
-    echo "4. 清除所有screen后台"
-    echo "v. 更新脚本"
-    echo "0. 退出脚本"
+    echo "          "
+    green "1. 创建screen后台并设置名称"
+    green "2. 查看并进入指定screen后台"
+    green "3. 查看并删除指定screen后台"
+    green "4. 清除所有screen后台"
+    green "0. 退出脚本"
     read -p "请输入选项:" menuNumberInput
     case "$menuNumberInput" in 
-        1 )
-            readp "设置screen后台名称：" screen
-            screen -S $screen
-            back;;
-        2 )
-            names=`screen -ls | grep '(Detached)' | awk '{print $1}' | awk -F "." '{print $2}'`
-            [[ -n $names ]] && green "$names" && readp "输入进入的screen后台名称：" screename && screen -r $screename || red "无执行内容"
-            back;;
-        3 )
-            names=`screen -ls | grep '(Detached)' | awk '{print $1}' | awk -F "." '{print $2}'`
-            [[ -n $names ]] && green "$names" && readp "输入删除的screen后台名称：" screename && screen -S $screename -X quit || red "无执行内容"
-            back;;
-        4 )
-            names=`screen -ls | grep '(Detached)' | awk '{print $1}' | awk -F "." '{print $2}'`
-            screen -wipe
-            [[ -n $names ]] && screen -ls | grep '(Detached)' | cut -d. -f1 | awk '{print $1}' | xargs kill && green "所有screen后台清除完毕"|| red "无执行内容，无须清除"
-            back;;
-        0 ) exit 0
+        1 ) createScreen ;;
+        2 ) enterScreen ;;
+        3 ) deleteScreen ;;
+        4 ) killAllScreen ;;
+        0 ) exit 1 ;;
     esac
 }
 
