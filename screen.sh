23dc8e47e2283ea5d87cd4670c5d0efb243f7b0b
Misaka No <96560028+misakano7545@users.noreply.github.com>
2022-06-17 22:44:44 +0800

    Update screen.sh

--- screen.sh
+++ screen.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+RED="\033[31m"
+GREEN="\033[32m"
+YELLOW="\033[33m"
+PLAIN='\033[0m'
+
 red() {
     echo -e "\033[31m\033[01m$1\033[0m"
 }
@@ -14,7 +19,7 @@ yellow() {
 
 REGEX=("debian" "ubuntu" "centos|red hat|kernel|oracle linux|alma|rocky" "'amazon linux'")
 RELEASE=("Debian" "Ubuntu" "CentOS" "CentOS")
-PACKAGE_UPDATE=("apt -y update" "apt -y update" "yum -y update" "yum -y update")
+PACKAGE_UPDATE=("apt-get -y update" "apt-get -y update" "yum -y update" "yum -y update")
 PACKAGE_INSTALL=("apt -y install" "apt -y install" "yum -y install" "yum -y install")
 PACKAGE_UNINSTALL=("apt -y autoremove" "apt -y autoremove" "yum -y autoremove" "yum -y autoremove")
 
@@ -32,11 +37,17 @@ done
 
 [[ -z $SYSTEM ]] && red "不支持当前VPS系统，请使用主流的操作系统" && exit 1
 
-[[ -z $(type -P screen) ]] && ${PACKAGE_UPDATE[int]} && ${PACKAGE_INSTALL[int]} screen
+if [[ -z $(type -P screen) ]]; then
+    if [[ ! $SYSTEM == "CentOS" ]]; then
+        ${PACKAGE_UPDATE[int]}
+    fi
+    ${PACKAGE_INSTALL[int]} screen
+fi
 
 back2menu() {
+    echo ""
     green "所选操作执行完成"
-    read -p "请输入“y”退出，或按任意键回到主菜单：" back2menuInput
+    read -rp "请输入“y”退出，或按任意键回到主菜单：" back2menuInput
     case "$back2menuInput" in
         y) exit 1 ;;
         *) menu ;;
@@ -44,24 +55,34 @@ back2menu() {
 }
 
 createScreen(){
-    read -p "设置screen后台名称：" screenName
-    screen -S $screenName
+    read -rp "设置screen后台会话名称：" screenName
+    if [[ -z $screenName ]]; then
+        red "未设置screen后台会话名称，退出操作"
+        back2menu
+    fi
+    screen -U -S $screenName
     back2menu
 }
 
 enterScreen(){
     screenNames=`screen -ls | grep '(Detached)' | awk '{print $1}' | awk -F "." '{print $2}'`
-    [[ -n $screenNames ]] && yellow "当前运行的Screen会话如下所示：" && green "$screenNames"
-    read -p "输入进入的screen后台名称：" screenName
-    screen -r $screenName || red "没有找到 $screenName 的会话"
+    if [[ -n $screenNames ]]; then
+        yellow "当前运行的Screen后台会话如下所示："
+        green "$screenNames"
+    fi
+    read -rp "输入进入的screen后台会话名称：" screenName
+    screen -r $screenName || red "没有找到 $screenName 会话"
     back2menu
 }
 
 deleteScreen(){
     screenNames=`screen -ls | grep '(Detached)' | awk '{print $1}' | awk -F "." '{print $2}'`
-    [[ -n $screenNames ]] && green "$screenNames"
-    read -p "输入删除的screen后台名称：" screenName
-    screen -S $screenName -X quit || red "没有找到 $screenName 的会话"
+    if [[ -n $screenNames ]]; then
+        yellow "当前运行的Screen后台会话如下所示："
+        green "$screenNames"
+    fi
+    read -rp "输入删除的screen后台会话名称：" screenName
+    screen -S $screenName -X quit || red "没有找到 $screenName 会话"
     back2menu
 }
 
@@ -75,28 +96,42 @@ killAllScreen(){
 
 menu(){
     clear
-    red "=================================="
-    echo "                           "
-    red "       Screen 后台运行管理脚本       "
-    red "          by 小御坂的破站           "
-    echo "                           "
-    red "  Site: https://owo.misaka.rest  "
-    echo "                           "
-    red "=================================="
-    echo "          "
-    green "1. 创建screen后台并设置名称"
-    green "2. 查看并进入指定screen后台"
-    green "3. 查看并删除指定screen后台"
-    green "4. 清除所有screen后台"
-    green "0. 退出脚本"
-    read -p "请输入选项:" menuNumberInput
+    echo "#############################################################"
+    echo -e "#                   ${RED} Screen  后台管理脚本${PLAIN}                   #"
+    echo -e "# ${GREEN}作者${PLAIN}: Misaka No                                           #"
+    echo -e "# ${GREEN}博客${PLAIN}: https://owo.misaka.rest                             #"
+    echo -e "# ${GREEN}论坛${PLAIN}: https://vpsgo.co                                    #"
+    echo -e "# ${GREEN}TG群${PLAIN}: https://t.me/misakanetcn                            #"
+    echo -e "# ${GREEN}GitHub${PLAIN}: https://github.com/Misaka-blog                    #"
+    echo -e "# ${GREEN}Bitbucket${PLAIN}: https://bitbucket.org/misakano7545             #"
+    echo -e "# ${GREEN}GitLab${PLAIN}: https://gitlab.com/misaka-blog                    #"
+    echo "#############################################################"
+    echo ""
+    echo -e " ${GREEN}1.${PLAIN} 创建screen后台会话并设置会话名称"
+    echo " -------------"
+    echo -e " ${GREEN}2.${PLAIN} 查看并进入指定screen后台会话"
+    echo -e " ${GREEN}3.${PLAIN} 查看并删除指定screen后台会话"
+    echo " -------------"
+    echo -e " ${GREEN}4.${PLAIN} 清除所有screen后台会话"
+    echo " -------------"
+    echo -e " ${GREEN}0.${PLAIN} 退出脚本"
+    echo ""
+    yellow "使用脚本的一些小提示："
+    yellow "1. 退出Screen后台会话时，请按Ctrl+A+D快捷键退出"
+    yellow "2. 请谨慎使用4选项"
+    echo ""
+    read -rp "请输入选项 [0-4]:" menuNumberInput
     case "$menuNumberInput" in 
         1 ) createScreen ;;
         2 ) enterScreen ;;
         3 ) deleteScreen ;;
         4 ) killAllScreen ;;
-        0 ) exit 1 ;;
+        * ) exit 1 ;;
     esac
 }
 
-menu
+if [ $# > 0 ]; then
+    menu
+else
+    menu
+fi
